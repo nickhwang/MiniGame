385a7c2693f20f57e9ed3c90b8d9dc99fbeb7927
NickHwang <31771958+nickhwang@users.noreply.github.com>
2018-10-30 16:56:56 +0800

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,1 +1,22 @@
+import random
 
+#1.在1～99隨機
+secret = random.randint(1,99)
+guess = 0
+tries = 0   
+print("AHOY! Come try my game！")
+
+#2.加入限制猜的次數
+while guess != secret and tries<6:
+    guess = int(input("what's your guess: "))
+    if guess < secret:
+        print('too low, Try again!')
+    elif guess > secret:
+        print('too high, Try again!')
+    tries = tries + 1
+    
+#3.贏&輸
+if guess == secret:
+    print('great, you win!')
+else:
+    print('bye, you lose!')
